658c5b4c504ed6ad8cf4d7b69e301fdbc9ca17f9
Vuk <vuk.tacic@gmail.com>
2025-08-12 15:43:59 -0700

    Add metadata for opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,25 +13,39 @@ export const metadata: Metadata = {
     keywords: ["Vuk", "Vuk Tacic", "Zylve", "Vancouver", "dsac", "bcydc", "creekside robotics", "7688", "creekside", "byrne creek", "burnaby"],
     icons: {
         icon: [
-          {
-            media: '(prefers-color-scheme: light)',
-            url: '/icons/icon-light.svg',
-            href: '/icons/icon-light.svg',
-          },
-          {
-            media: '(prefers-color-scheme: dark)',
-            url: '/icons/icon.svg',
-            href: '/icons/icon.svg',
-          },
+            {
+                media: '(prefers-color-scheme: light)',
+                url: '/icons/icon-light.svg',
+                href: '/icons/icon-light.svg',
+            },
+            {
+                media: '(prefers-color-scheme: dark)',
+                url: '/icons/icon.svg',
+                href: '/icons/icon.svg',
+            },
         ],
-      },
+    },
+
+    openGraph: {
+        title: 'Vuk',
+        description: 'Twelfth grade student from vancouver, bc',
+        url: 'https://vuktacic.com',
+        siteName: 'Vuk Tacic',
+        images: [
+            {
+                url: 'https://vuktacic.com/og.png', // Must be an absolute URL
+                width: 2048,
+                height: 629,
+            },
+        ],
+    },
 };
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
-    return (
-        <>
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Z5XKY87YL8" />
-            <Script id="google-analytics">{`
+    export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
+        return (
+            <>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Z5XKY87YL8" />
+                <Script id="google-analytics">{`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
@@ -40,13 +54,13 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             `}</Script>
 
 
-            <html lang="en">
-                <body
-                    className={` ${montserrat.variable} antialiased bg-zinc-900 text-white min-h-screen font-sans overflow-x-hidden`}
-                >
-                    {children}
-                </body>
-            </html>
-        </>
-    );
+                <html lang="en">
+                    <body
+                        className={` ${montserrat.variable} antialiased bg-zinc-900 text-white min-h-screen font-sans overflow-x-hidden`}
+                    >
+                        {children}
+                    </body>
+                </html>
+            </>
+        );
 }
